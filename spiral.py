6ed272abf902cb2eb95d8362d64485c3f1b7acd5
Kevin Howell <kevin@kahowell.net>
2013-10-11 23:22:07 -0400

    Add arg length check

--- spiral.py
+++ spiral.py
@@ -90,4 +90,7 @@ class SpiralPrinter(object):
             print('')
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage:', sys.argv[0], '<number>')
+        sys.exit(1)
     SpiralPrinter(int(sys.argv[1])).print()
